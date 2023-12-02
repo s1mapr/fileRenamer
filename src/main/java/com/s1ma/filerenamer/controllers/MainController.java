8c899b0ee7ad003bc2e35b83a19fa41c53fed9d3
S1ma <s1maprokopenko@gmail.com>
2023-12-02 14:31:54 +0200

    added downloading and processing files on server side

--- src/main/java/com/s1ma/filerenamer/controllers/MainController.java
+++ src/main/java/com/s1ma/filerenamer/controllers/MainController.java
@@ -2,11 +2,19 @@ package com.s1ma.filerenamer.controllers;
 
 import com.s1ma.filerenamer.services.RenameFilesService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,17 +23,42 @@ public class MainController {
     private final RenameFilesService renameFileService;
 
     @GetMapping("/")
-    public String getMainPage(){
+    public String getMainPage() {
         return "index";
     }
 
-    @PostMapping
-    public String renameAllFiles(@RequestParam("pattern") String pattern,
-                                 @RequestParam("filesPath") String filePath,
-                                 HttpServletRequest request){
+    @PostMapping("/")
+    public void renameAllFiles(@RequestParam("pattern") String pattern,
+                                       @RequestParam("files") MultipartFile[] files,
+                                       HttpServletRequest request,
+                                       HttpServletResponse response) throws IOException {
         String lineEnds = request.getParameter("lineEnds");
-        renameFileService.readFilesMethod(filePath, pattern, lineEnds);
-        return "redirect:/";
+        String zipName = renameFileService.readFilesMethod(files, pattern, lineEnds);
+        String uploadPath = "target/uploads";
+
+        File zipFile = new File(uploadPath, zipName);
+
+        response.setContentType("application/zip");
+        response.setHeader("Content-Disposition", "attachment; filename=" + zipName);
+
+        try (FileInputStream fis = new FileInputStream(zipFile);
+             ZipOutputStream zos = new ZipOutputStream(response.getOutputStream())) {
+
+            ZipEntry zipEntry = new ZipEntry(zipFile.getName());
+            zos.putNextEntry(zipEntry);
+
+            byte[] bytes = new byte[1024];
+            int length;
+            while ((length = fis.read(bytes)) >= 0) {
+                zos.write(bytes, 0, length);
+            }
+
+            zos.closeEntry();
+        }
+
+        zipFile.delete();
+
+        response.flushBuffer();
     }
 
 }
--- src/main/java/com/s1ma/filerenamer/services/RenameFilesService.java
+++ src/main/java/com/s1ma/filerenamer/services/RenameFilesService.java
@@ -1,39 +1,39 @@
 package com.s1ma.filerenamer.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Service
 public class RenameFilesService {
-    public void readFilesMethod(String folderPath, String pattern, String lineEnds) {
-
-        File folder = new File(folderPath);
-
-        File[] files = folder.listFiles();
+    public String readFilesMethod(MultipartFile[] files, String pattern, String lineEnds) {
 
         String[] array = parseString(lineEnds);
-
+        List<String> fileNames = new ArrayList<>();
         if (files != null && files.length == array.length) {
             for (int i = 0; i < files.length; i++) {
-                File file = files[i];
-                String[] parsedFileName = file.getName().split("\\.");
+                MultipartFile file = files[i];
+                String[] parsedFileName = file.getOriginalFilename().split("\\.");
                 String fileType = "." + parsedFileName[parsedFileName.length-1];
                 String newName = pattern + array[i] + fileType;
-                if (file.renameTo(new File(folder, newName))) {
-                    System.out.println("Файл успешно переименован: " + newName);
-                } else {
-                    System.out.println("Не удалось переименовать файл: " + file.getName());
-                }
+                fileNames.add(newName);
+                saveFile(file, newName);
             }
-        } else {
-            System.out.println("Папка пуста или произошла ошибка при чтении файлов.");
         }
 
+        String zipFileName = createZipFile(fileNames);
+        deleteFiles(fileNames);
+        return zipFileName;
     }
 
     private String[] parseString(String input) {
@@ -48,5 +48,60 @@ public class RenameFilesService {
         }
 
         return elements.toArray(new String[0]);
+    }
+
+    private void saveFile(MultipartFile file, String newFileName) {
+        try {
+            byte[] bytes = file.getBytes();
+            String uploadPath = "target/uploads";
+
+            File newFile = new File(uploadPath + File.separator + newFileName);
+            try (FileOutputStream stream = new FileOutputStream(newFile)) {
+                stream.write(bytes);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("something went wrong");
+        }
+    }
+
+    private String createZipFile(List<String> fileNames) {
+        String zipFileName = "archive.zip";
+        String uploadPath = "target/uploads";
+
+        try (FileOutputStream fos = new FileOutputStream(uploadPath + File.separator + zipFileName);
+             ZipOutputStream zos = new ZipOutputStream(fos)) {
+
+            for (String fileName : fileNames) {
+                File file = new File(uploadPath + File.separator + fileName);
+                ZipEntry zipEntry = new ZipEntry(fileName);
+                zos.putNextEntry(zipEntry);
+
+                byte[] bytes = new byte[1024];
+                int length;
+                try (FileInputStream fis = new FileInputStream(file)) {
+                    while ((length = fis.read(bytes)) >= 0) {
+                        zos.write(bytes, 0, length);
+                    }
+                }
+                zos.closeEntry();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return zipFileName;
+    }
+
+    private void deleteFiles(List<String> fileNames) {
+        String uploadPath = "target/uploads";
+
+        for (String fileName : fileNames) {
+            File file = new File(uploadPath + File.separator + fileName);
+            if (file.exists()) {
+                file.delete();
+            }
+        }
     }
 }
